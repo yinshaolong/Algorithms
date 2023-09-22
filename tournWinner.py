da3dc2a7ced5015c416cfc2365830d6a00aa9007
yinshaolong <syin18@my.bcit.ca>
2023-09-21 22:19:33 -0700

    unanswered question

--- tournWinner.py
+++ tournWinner.py
@@ -1,1 +1,11 @@
-def tournWinner()
+def tournWinner(array, sequence):
+    pass
+
+competition = [
+    ["HTML", "CSS"],
+    ["CSS", "Python"],
+     ["Python", "Java"]
+]
+results = [0, 0, 1]
+
+print(tournWinner(competition, results))
